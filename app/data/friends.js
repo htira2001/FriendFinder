a6986d0c992a79b7fd88097a207cf7687524be8f
helmi <helmitira@yahoo.com>
2019-02-21 13:30:53 -0500

    adding images and friends

---
+++ app/data/friends.js
@@ -0,0 +1,137 @@
+// Creating an array with friends data //
+
+var friendsData = [
+
+    {
+
+        name: "sam",
+
+        photo: "sam.jpg",
+
+        scores: [
+
+            "4",
+
+            "1",
+
+            "2",
+
+            "4",
+
+            "5",
+
+            "5",
+
+            "3",
+
+            "2",
+
+            "4",
+
+            "5"
+
+        ]
+
+    },
+
+    {
+
+        name: "bruno",
+
+        photo: "bruno.jpg",
+
+        scores: [
+
+            "3",
+
+            "1",
+
+            "5",
+
+            "2",
+
+            "3",
+
+            "4",
+
+            "5",
+
+            "1",
+
+            "2",
+
+            "5"
+
+        ]
+
+    },
+
+    {
+
+        name: "Alex",
+
+        photo: "Alex.jpg",
+
+        scores: [
+
+            "4",
+
+            "5",
+
+            "3",
+
+            "2",
+
+            "5",
+
+            "4",
+
+            "2",
+
+            "5",
+
+            "1",
+
+            "4"
+
+        ]
+
+    },
+
+    {
+
+        name: "alejandro",
+
+        photo: "alejandro.jpg",
+
+        scores: [
+
+            "4",
+
+            "5",
+
+            "1",
+
+            "2",
+
+            "3",
+
+            "4",
+
+            "5",
+
+            "2",
+
+            "4",
+
+            "3"
+
+        ]
+
+    }
+
+];
+
+
+
+module.exports = friendsData;
